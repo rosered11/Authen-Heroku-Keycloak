2cc993723b26701b9704cd78d583c44cc650cc55
Rosered <krihsana5@gmail.com>
2021-04-18 14:47:53 +0700

    remove unuse function

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -1,10 +1,6 @@
 ﻿using Authentication.DataAccess.Model;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Authentication.DataAccess
 {
